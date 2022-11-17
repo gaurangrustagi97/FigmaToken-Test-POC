fc6c50c5428d5fc66e25bef535399ff8cfc86848
Gaurang Rustagi <Gaurang.Rustagi@merkleinc.com.au>
2022-11-18 00:16:10 +1100

    added colors

---
+++ theme/color.ts
@@ -0,0 +1,13 @@
+export const colors = {
+  strike: {
+    "action-1": "red",
+    "action-2": "yellow",
+  },
+  holeyMoley: {
+    "action-1": "blue",
+    "action-2": "green",
+  },
+  core: {
+    black: "black",
+  },
+};
